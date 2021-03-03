26ba66fb9f914eeb95f939b0d714307dfa0d0207
Lucca <adrielpanero@gmail.com>
2021-03-02 21:30:05 -0300

    primer commit repositorio local

---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+export const urlApiProducts = "https://demo2562028.mockable.io/products"; //https://jsonserve.com/
+export const BASE_PATH = "http://localhost:3000";
+export const STORAGE_PRODUCTS_CART = "productCart";
